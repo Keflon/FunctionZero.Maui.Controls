cf4b7a5d4940439c92e041328749816e6fb7c5be
Keith Pickford <Keith@FunctionZero.com>
2022-09-07 16:17:49 +0100

    Refactor.

--- FunctionZero.Maui.Controls/Platforms/Tizen/PlatformSetup.cs
+++ FunctionZero.Maui.Controls/Platforms/Tizen/PlatformSetup.cs
@@ -3,7 +3,7 @@
 namespace FunctionZero.Maui.Controls
 {
     // All the code in this file is only included on Tizen.
-    public class PlatformClass1
+    public partial class PlatformSetup
     {
     }
 }
