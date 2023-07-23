c46191c762c322f692497ecc0d36b0c8acea963c
Keith Pickford <Keith@FunctionZero.com>
2023-07-23 22:34:54 +0100

    MaskControl can track things on screen by name.

--- SampleApp/Mvvm/PageViewModels/Mask/CircleMaskPageVm.cs
+++ SampleApp/Mvvm/PageViewModels/Mask/CircleMaskPageVm.cs
@@ -9,9 +9,32 @@ namespace SampleApp.Mvvm.PageViewModels.Mask
 {
     public class CircleMaskPageVm : BasePageVm
     {
+        private string _targetName;
+
+        public string TargetName
+        {
+            get => _targetName;
+            set => SetProperty(ref _targetName, value);
+        }
+
         public CircleMaskPageVm()
         {
-            
+            _ = DoTheThingAsync();
+        }
+
+        private async Task DoTheThingAsync()
+        {
+            while (true)
+            {
+                await Task.Delay(3000);
+                TargetName = "banana";
+                await Task.Delay(3000);
+                TargetName = "radish";
+                await Task.Delay(3000);
+                TargetName = "melon";
+                await Task.Delay(3000);
+                TargetName = "grapefruit";
+            }
         }
     }
 }
