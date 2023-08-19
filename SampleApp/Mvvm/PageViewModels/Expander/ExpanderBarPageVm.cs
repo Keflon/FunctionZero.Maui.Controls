a21c037d6a8671008b85016907cf4c2c46e5cf80
Keith Pickford <Keith@FunctionZero.com>
2023-08-19 17:27:09 +0100

    ExpanderBarPage has a button to swap light / dark theme.

--- SampleApp/Mvvm/PageViewModels/Expander/ExpanderBarPageVm.cs
+++ SampleApp/Mvvm/PageViewModels/Expander/ExpanderBarPageVm.cs
@@ -1,13 +1,33 @@
-﻿using SampleApp.Mvvm.ViewModels;
+﻿using FunctionZero.CommandZero;
+using SampleApp.Mvvm.ViewModels;
+using SampleApp.Services;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace SampleApp.Mvvm.PageViewModels.Expander
 {
     public class ExpanderBarPageVm : BasePageVm
     {
+        public ExpanderBarPageVm()
+        {
+            ThemeButtonCommand = new CommandBuilder().SetExecuteAsync(ThemeButtonCommandExecuteAsync).Build();
+        }
+
+        private async Task ThemeButtonCommandExecuteAsync(object arg)
+        {
+            if(Application.Current.UserAppTheme == AppTheme.Dark)
+                Application.Current.UserAppTheme = AppTheme.Light; 
+            else
+                Application.Current.UserAppTheme = AppTheme.Dark;
+        }
+
+        public ICommand ThemeButtonCommand { get; }
+
+        public SampleAppThemeService ThemeService { get; }
+
     }
 }
---
+++ SampleApp/Services/ThemeService.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleApp.Services
+{
+
+
+    public class ThemeService<T> where T : Enum
+    {
+        public ThemeService()
+        {
+        }
+    }
+
+    public enum ThemeType
+    {
+        None,
+        Light,
+        Dark,
+        Developer,
+        HighVisibilityLight,
+        HighVisibilityDark,
+        Disco
+    }
+
+    public class SampleAppThemeService : ThemeService<ThemeType>
+    {
+        public SampleAppThemeService()
+        {
+            this.CurrentThemeType = ThemeType.Disco;
+        }
+
+        public ThemeType CurrentThemeType;
+    }
+
+    public class Radish
+    {
+        public Radish(SampleAppThemeService sampleAppThemeService)
+        {
+            sampleAppThemeService.CurrentThemeType = ThemeType.Dark;
+        }
+    }
+}
